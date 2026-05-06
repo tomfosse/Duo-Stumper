352ac65d8ab8d3d3281de09c521ff8302bd2015a
Tom Fossé <tom.fosse@epitech.eu>
2026-05-06 10:45:08 +0200

    add(cesar.h) the main header file of the project

---
+++ include/cesar.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2026
+** Cesar
+** File description:
+** The main header file of the project
+*/
+
+#ifndef CESAR_H
+    #define CESAR_H
+
+#define EXIT_ERROR 84;
+
+
+#endif
