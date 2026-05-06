dddab155ac3ad8d14f6fb5ee49ebbf3f625504f8
Tom Fossé <tom.fosse@epitech.eu>
2026-05-06 11:41:22 +0200

    add(encrypt_handler) the function to handle the encrypt, char by char

---
+++ src/handlers/encrypt/encrypt_handler.c
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2026
+** Cesar
+** File description:
+** The function to encrypt the char * str
+*/
+
+#include <string.h>
+#include <stdlib.h>
+#include "cesar.h"
+
+char *encrypt_str(char *str, int key)
+{
+    char *output = NULL;
+    int len = 0;
+
+    if (!str)
+        return NULL;
+    if (key == 0)
+        return str;
+    len = strlen(str);
+    output = malloc(sizeof(char) * (len + 1));
+    if (!output)
+        return NULL;
+    if (key < 0)
+        for (int i = 0; str[i] != '\0'; i++)
+            output[i] = encrypt_negative_char(key, str[i]);
+    if (key > 0)
+        for (int i = 0; str[i] != '\0'; i++)
+            output[i] = encrypt_positive_char(key, str[i]);
+    output[len] = '\0';
+    return output;
+}
