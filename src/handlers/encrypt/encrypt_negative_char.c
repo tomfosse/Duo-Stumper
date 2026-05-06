2739f8e9223c6487f60d42e9a265c87ab31fb71b
Tom Fossé <tom.fosse@epitech.eu>
2026-05-06 11:41:49 +0200

    add(encrypt_negative_char) the function to handle the deplacement of a char when the key is negative

---
+++ src/handlers/encrypt/encrypt_negative_char.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2026
+** Cesar
+** File description:
+** The function to encrypt a negative key char
+*/
+
+#include "cesar.h"
+
+char encrypt_negative_char(int key, char c)
+{
+    if (my_char_is_alpha(c) != 1)
+        return c;
+    for (int i = key; i < 0; i++) {
+        if (c == 'A') {
+            c = 'Z';
+            continue;
+        }
+        if (c == 'a') {
+            c = 'Z';
+            continue;
+        }
+        c--;
+    }
+    return c;
+}
