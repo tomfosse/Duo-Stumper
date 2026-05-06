90bfc05d1ead8dd94be22e942c7745adf2fda3b9
Tom Fossé <tom.fosse@epitech.eu>
2026-05-06 13:02:36 +0200

    fix(my_is_num) the error handling

--- src/utils/my_is_num.c
+++ src/utils/my_is_num.c
@@ -5,13 +5,16 @@
 ** my is num
 */
 
+#include <stdlib.h>
 #include "cesar.h"
 
 int my_is_num(char *str)
 {
     for (int i = 0; str[i]; i++) {
+        if (str[i] == '-')
+            continue;
         if (str[i] < '0' || str[i] > '9')
-            return EXIT_ERROR; 
+            return EXIT_ERROR;
     }
     return EXIT_SUCCESS;
 }
