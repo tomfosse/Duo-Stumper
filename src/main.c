51957acb57c4eec65c03fce664e9f996a6fd9c5d
Tom Fossé <tom.fosse@epitech.eu>
2026-05-06 12:41:24 +0200

    add(main) the handling of writing in the file

--- src/main.c
+++ src/main.c
@@ -11,35 +11,41 @@
 #include "cesar.h"
 
 static
-void file_flag_handling()
+int file_flag_handling(char *filepath, int key)
 {
-    file_datas = parse_file(av[2]);
+    char *file_datas = NULL;
+    char *string = NULL;
+
+    file_datas = parse_file(filepath);
+    if (!file_datas)
+        return EXIT_ERROR;
     string = encrypt_str(file_datas, key);
-    //Write in file function
+    if (!string)
+        return EXIT_ERROR;
+    if (write_into_file(string) == EXIT_ERROR)
+        return EXIT_ERROR;
     free(string);
     free(file_datas);
+    return EXIT_SUCCESS;
 }
 
 int main(int ac, char **av)
 {
     int key = 0;
-    char *file_datas = NULL;
     char *string = NULL;
-    
+
     if (ac != 4)
         return EXIT_ERROR;
     key = atoi(av[3]);
     if (strcmp(av[1], "-f") == EXIT_SUCCESS) {
-        file_datas = parse_file(av[2]);
-        string = encrypt_str(file_datas, key);
-        //Write in file function
-        free(string);
-        free(file_datas);
+        if (file_flag_handling(av[2], key) == EXIT_ERROR)
+            return EXIT_ERROR;
         return EXIT_SUCCESS;
     }
     if (strcmp(av[1], "-s") == EXIT_SUCCESS) {
         string = encrypt_str(av[2], key);
-        //Write in file function
+        if (!string || write_into_file(string) == EXIT_ERROR)
+            return EXIT_ERROR;
         free(string);
         return EXIT_SUCCESS;
     }
