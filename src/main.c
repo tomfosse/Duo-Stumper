30ed75c86934a6b1fc8eadcaef4241ef5eaa6ef5
Tom Fossé <tom.fosse@epitech.eu>
2026-05-06 11:40:25 +0200

    add(main) the handle of the program's logic

--- src/main.c
+++ src/main.c
@@ -5,7 +5,30 @@
 ** The main function's file of the project
 */
 
-int main(void)
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "cesar.h"
+
+int main(int ac, char **av)
 {
-    return 0;
+    int key = 0;
+    char *string = NULL;
+    
+    if (ac != 4)
+        return EXIT_ERROR;
+    key = atoi(av[3]);
+    if (strcmp(av[1], "-f") == EXIT_SUCCESS) {
+        //string = Get the char * from file AND encrypt
+        //Write in file function
+        free(string);
+        return EXIT_SUCCESS;
+    }
+    if (strcmp(av[1], "-s") == EXIT_SUCCESS) {
+        string = encrypt_str(av[2], key);
+        //Write in file function
+        free(string);
+        return EXIT_SUCCESS;
+    }
+    return EXIT_ERROR;
 }
