537f648931492f464d20e4edf1ab0bbe07e3d901
Dimitri <dimitri.jouanneau@epitech.eu>
2026-05-06 12:46:34 +0200

    feat(my_is_num): check if it's a number

---
+++ src/utils/my_is_num.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** my_is_num.c
+** File description:
+** my is num
+*/
+
+#include "cesar.h"
+
+int my_is_num(char *str)
+{
+    for (int i = 0; str[i]; i++) {
+        if (str[i] < '0' || str[i] > '9')
+            return EXIT_ERROR; 
+    }
+    return EXIT_SUCCESS;
+}
